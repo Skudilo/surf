7726c3cc6e76364d294ca5cfe7aac85a977dc580
skudilo <skudilo@gmail.com>
2019-10-30 14:53:58 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows--left" src="img/arrow-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows--right" src="img/arrow-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots'
+    })
+    
+    $('.slider-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    });
+
+});
