73f8ddc731fca62dd7285f273f85fa8dc166f346
skudilo <skudilo@gmail.com>
2019-11-01 13:10:04 +0200

    note

--- app/js/main.js
+++ app/js/main.js
@@ -5,19 +5,29 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows--left" src="img/arrow-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows--right" src="img/arrow-right.svg" alt=""></img>',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     })
     
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider'
     });
+
     $('.surf-slider').slick({
         prevArrow: '<img class="slider-arrows slider-arrows--left" src="img/arrow-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows--right" src="img/arrow-right.svg" alt=""></img>',
         slidesToShow: 4,
-        slidesToScroll: 1
-    })
+        slidesToScroll: 1,
+        asNavFor: '.slider-map'
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slideToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect: true
+    }) ;
 });
 
